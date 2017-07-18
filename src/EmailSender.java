7df900ea7aef9e874b7b01a0dc0f2b7a93430cf2
xumingming <mingming.xu@fangbaba.com>
2017-07-18 15:56:57 +0800

    test

--- src/EmailSender.java
+++ src/EmailSender.java
@@ -3,6 +3,8 @@ public class EmailSender {
 
 	public static void main(String args[]){
 
-		System.out.println("yyyyyyyyyyyyxxx111");
+		System.out.println("yyyyyyyyyyyyxxx11145");
+
+		System.out.println("22222222222");
 	}
 }
